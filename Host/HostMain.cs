8483c8aa6d209aac88ea1329f1ec3378cc725943
jamesw1123 <jamesw11233@gmail.com>
2022-08-30 14:44:52 -0500

    Add a project to host the service.
    Created the app configuration file.
    Added an initial SQLite db file.

---
+++ Host/HostMain.cs
@@ -0,0 +1,33 @@
+﻿using DataComm.DbAccess;
+using DataComm.Models;
+using System;
+using System.ServiceModel;
+
+namespace Host
+{
+    internal class HostMain
+    {
+        private static void Main(string[] args)
+        {
+            MessageModel message = new MessageModel();
+            message.Name = "Anyone Else";
+            message.Address = "Some St.";
+            message.Phone = "987-987-9876";
+            message.Email = "anyone@email.com";
+            message.MessageText = "ANY message can go here";
+            message.DateTaken = DateTime.Now.ToString();
+            message.Recipient = "user3";
+            message.SentFrom = "user1";
+
+            DB.Insert(message);
+            DB.Read();
+
+            using (ServiceHost host = new ServiceHost(typeof(MessageService.MessageService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
