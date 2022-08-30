667007af351e50be60f38702f22c536e440bba08
jamesw1123 <jamesw11233@gmail.com>
2022-08-30 14:39:23 -0500

    Added a model for message

---
+++ DataComm/Models/MessageModel.cs
@@ -0,0 +1,16 @@
+﻿namespace DataComm.Models
+{
+    public class MessageModel
+    {
+        public string Mid { get; set; }
+        public string Recipient { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string MessageText { get; set; }
+        public string DateTaken { get; set; }
+        public int Delivered { get; set; }
+        public string SentFrom { get; set; }
+    }
+}
