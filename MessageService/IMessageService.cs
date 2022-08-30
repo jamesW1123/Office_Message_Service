37e1a31862559b3c5cd396fd4180d7aa21a9b4ad
jamesw1123 <jamesw11233@gmail.com>
2022-08-30 14:38:46 -0500

    Added files for the service.

---
+++ MessageService/IMessageService.cs
@@ -0,0 +1,31 @@
+﻿using DataComm.Models;
+using System.ServiceModel;
+
+namespace MessageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract(CallbackContract = typeof(IMessageServiceCallback))]
+    public interface IMessageService
+    {
+        [OperationContract(IsOneWay = true)]
+        void ProcessReport();
+
+        [OperationContract]
+        MessageModel GetMessage(string id);
+
+        [OperationContract(IsOneWay = true)]
+        void Join(string userId);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMessage(MessageModel message);
+    }
+
+    public interface IMessageServiceCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void Progress(int percentageCompleted);
+
+        [OperationContract(IsOneWay = true)]
+        void RecieveMessage(MessageModel message);
+    }
+}
---
+++ MessageService/MessageService.cs
@@ -0,0 +1,63 @@
+﻿using DataComm.Models;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading;
+
+namespace MessageService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
+    public class MessageService : IMessageService
+    {
+        private Dictionary<string, IMessageServiceCallback> users = new Dictionary<string, IMessageServiceCallback>();
+
+        public MessageModel GetMessage(string id)
+        {
+            //DBComms.GetMessages();
+
+            MessageModel message = new MessageModel();
+            message.Name = "Someone Else";
+            message.Address = "123 Any St.";
+            message.Phone = "123-123-1234";
+            message.Email = "someone@email.com";
+            message.MessageText = "Some message goes here";
+            message.DateTaken = DateTime.Now.ToString();
+            message.Recipient = id;
+
+            return message;
+        }
+
+        public void Join(string userId)
+        {
+            users.Add(userId, OperationContext.Current.GetCallbackChannel<IMessageServiceCallback>());
+        }
+
+        public void ProcessReport()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread.Sleep(50);
+                OperationContext.Current.GetCallbackChannel<IMessageServiceCallback>().Progress(i);
+            }
+        }
+
+        public void SendMessage(MessageModel message)
+        {
+            Console.WriteLine(message.Name);
+            Console.WriteLine(message.Address);
+            Console.WriteLine(message.Phone);
+            Console.WriteLine(message.Email);
+            Console.WriteLine(message.MessageText);
+            Console.WriteLine(message.DateTaken);
+            Console.WriteLine(message.Recipient);
+
+            //DBComms.Insert(message);
+
+            string rec = message.Recipient;
+
+            var con = users[rec];
+            con.RecieveMessage(message);
+        }
+    }
+}
