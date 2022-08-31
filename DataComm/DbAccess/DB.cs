e3f2394ff6bf7fe8ae7158916a5438b0a30813ac
jamesw1123 <jamesw11233@gmail.com>
2022-08-31 12:43:44 -0500

    Made the connection string a form level, read-only variable. No need to go get it for every operation, since it doesn't change.
    Wrapped all of the SQL commands in try-catch blocks.
    Add functions to mark a message delivered and not delivered.
    Removed the GenerateMid function, since it is no longer needed.
    Added a function to get messages for a recipient.

--- DataComm/DbAccess/DB.cs
+++ DataComm/DbAccess/DB.cs
@@ -1,66 +1,122 @@
 ﻿using Dapper;
 using DataComm.Models;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SQLite;
+using System.Linq;
 
 namespace DataComm.DbAccess
 {
     public class DB
     {
+        private static readonly string _connectionString = ConfigurationManager.ConnectionStrings["Default"].ConnectionString;
+
         public static void Insert(MessageModel message)
         {
-            //Message message = new Message();
-            //message.Name = "Someone Else";
-            //message.Address = "123 Any St.";
-            //message.Phone = "123-123-1234";
-            //message.Email = "someone@email.com";
-            //message.MessageText = "Some message goes here";
-            //message.DateTaken = DateTime.Now.ToString();
-            //message.Recipient = "user2";
-
-            message.Mid = GenerateMid(message);
-
-            string sql = "INSERT INTO Messages" +
-                "(mid, recipient, name, address, phone, email, message_text, date_taken, sent_from, delivered) " +
-                "VALUES(@Mid, @Recipient, @Name, @Address, @Phone, @Email, @MessageText, @DateTaken, @SentFrom, @Delivered)";
-
-            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            try
             {
-                conn.Execute(sql, message);
+                using (IDbConnection conn = new SQLiteConnection(_connectionString))
+                {
+                    string sql = "INSERT INTO Messages" +
+                        "(recipient, name, address, phone, email, message_text, date_taken, sent_from, delivered) " +
+                        "VALUES(@Recipient, @Name, @Address, @Phone, @Email, @Message_Text, @Date_Taken, @Sent_From, @Delivered)";
+
+                    conn.Execute(sql, message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void MarkDelivered(int mid)
+        {
+            try
+            {
+                using (IDbConnection conn = new SQLiteConnection(_connectionString))
+                {
+                    string sql = "UPDATE Messages" +
+                        "SET delivered = 1" +
+                        "WHERE mid = @Mid";
+
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@recipient", mid);
+
+                    conn.Execute(sql, parameters);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static void MarkNotDelivered(int mid)
+        {
+            try
+            {
+                using (IDbConnection conn = new SQLiteConnection(_connectionString))
+                {
+                    string sql = "UPDATE Messages" +
+                        "SET delivered = 0" +
+                        "WHERE mid = @Mid";
+
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@recipient", mid);
+
+                    conn.Execute(sql, parameters);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
             }
         }
 
         public static void Read()
         {
-            using (IDbConnection conn = new SQLiteConnection(LoadConnectionString()))
+            using (IDbConnection conn = new SQLiteConnection(_connectionString))
             {
                 var output = conn.Query<MessageModel>("SELECT * FROM messages", new DynamicParameters());
                 foreach (MessageModel msg in output)
                 {
-                    Console.WriteLine(msg.Mid);
-                    Console.WriteLine(msg.Recipient);
-                    Console.WriteLine(msg.Name);
-                    Console.WriteLine(msg.Address);
-                    Console.WriteLine(msg.Phone);
-                    Console.WriteLine(msg.Email);
-                    Console.WriteLine(msg.MessageText);
-                    Console.WriteLine(msg.DateTaken);
-                    Console.WriteLine(msg.Delivered);
-                    Console.WriteLine(msg.SentFrom);
+                    //Console.WriteLine(msg.Mid);
+                    //Console.WriteLine(msg.Recipient);
+                    //Console.WriteLine(msg.Name);
+                    //Console.WriteLine(msg.Address);
+                    //Console.WriteLine(msg.Phone);
+                    //Console.WriteLine(msg.Email);
+                    //Console.WriteLine(msg.MessageText);
+                    //Console.WriteLine(msg.DateTaken);
+                    //Console.WriteLine(msg.Delivered);
+                    //Console.WriteLine(msg.SentFrom);
                 }
             }
         }
 
-        private static string LoadConnectionString(string id = "Default")
+        public static List<MessageModel> GetMessages(string recipient)
         {
-            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
-        }
+            try
+            {
+                using (IDbConnection conn = new SQLiteConnection(_connectionString))
+                {
+                    string sql = "SELECT * FROM messages WHERE recipient = @recipient";
 
-        private static string GenerateMid(MessageModel message)
-        {
-            return message.GetHashCode().ToString();
+                    var parameters = new DynamicParameters();
+                    parameters.Add("@recipient", recipient);
+
+                    var output = conn.Query<MessageModel>(sql, parameters);
+                    return output.ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return null;
+            }
         }
     }
 }
