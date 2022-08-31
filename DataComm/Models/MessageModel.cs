bbc46fc0d6f59a5c44f60771c2ee6ff8d5f6988c
jamesw1123 <jamesw11233@gmail.com>
2022-08-31 12:29:45 -0500

    Modified the message model so that the property names match the column names in the db. Changed the Mid property to have a private setter, since it is now an auto-incremented id field.

--- DataComm/Models/MessageModel.cs
+++ DataComm/Models/MessageModel.cs
@@ -2,15 +2,15 @@
 {
     public class MessageModel
     {
-        public string Mid { get; set; }
+        public string Mid { get; private set; }
         public string Recipient { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public string MessageText { get; set; }
-        public string DateTaken { get; set; }
+        public string Message_Text { get; set; }
+        public string Date_Taken { get; set; }
         public int Delivered { get; set; }
-        public string SentFrom { get; set; }
+        public string Sent_From { get; set; }
     }
 }
