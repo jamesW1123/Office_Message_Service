4a40d5ec03d22aa419add9f0b6e51cf2b91c1fa3
jamesw1123 <jamesw11233@gmail.com>
2022-08-31 12:44:26 -0500

    Changed and added testing code.

--- Host/HostMain.cs
+++ Host/HostMain.cs
@@ -14,13 +14,27 @@ namespace Host
             message.Address = "Some St.";
             message.Phone = "987-987-9876";
             message.Email = "anyone@email.com";
-            message.MessageText = "ANY message can go here";
-            message.DateTaken = DateTime.Now.ToString();
+            message.Message_Text = "ANY message can go here";
+            message.Date_Taken = DateTime.Now.ToString();
             message.Recipient = "user3";
-            message.SentFrom = "user1";
+            message.Sent_From = "user1";
 
             DB.Insert(message);
             DB.Read();
+            var x = DB.GetMessages("user3");
+            foreach (MessageModel msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Recipient);
+                Console.WriteLine(msg.Name);
+                Console.WriteLine(msg.Address);
+                Console.WriteLine(msg.Phone);
+                Console.WriteLine(msg.Email);
+                Console.WriteLine(msg.Message_Text);
+                Console.WriteLine(msg.Date_Taken);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Sent_From);
+            }
 
             using (ServiceHost host = new ServiceHost(typeof(MessageService.MessageService)))
             {
