9edd44d5af365f3ba46da23093a37f971d98e154
jamesw1123 <jamesw11233@gmail.com>
2022-09-01 10:06:23 -0500

    Added more testing code

--- Host/HostMain.cs
+++ Host/HostMain.cs
@@ -9,18 +9,17 @@ namespace Host
     {
         private static void Main(string[] args)
         {
-            MessageModel message = new MessageModel();
-            message.Name = "Anyone Else";
-            message.Address = "Some St.";
-            message.Phone = "987-987-9876";
-            message.Email = "anyone@email.com";
-            message.Message_Text = "ANY message can go here";
-            message.Date_Taken = DateTime.Now.ToString();
-            message.Recipient = "user3";
-            message.Sent_From = "user1";
+            //MessageModel message = new MessageModel();
+            //message.Name = "Anyone Else";
+            //message.Address = "Some St.";
+            //message.Phone = "987-987-9876";
+            //message.Email = "anyone@email.com";
+            //message.Message_Text = "ANY message can go here";
+            //message.Date_Taken = DateTime.Now.ToString();
+            //message.Recipient = "user3";
+            //message.Sent_From = "user1";
+            //DB.Insert(message);
 
-            DB.Insert(message);
-            DB.Read();
             var x = DB.GetMessages("user3");
             foreach (MessageModel msg in x)
             {
@@ -32,8 +31,59 @@ namespace Host
                 Console.WriteLine(msg.Email);
                 Console.WriteLine(msg.Message_Text);
                 Console.WriteLine(msg.Date_Taken);
-                Console.WriteLine(msg.Delivered);
                 Console.WriteLine(msg.Sent_From);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
+
+                DB.MarkRead(msg.Mid);
+            }
+            Console.WriteLine("______________________________________________________________________________________________");
+            x = DB.GetMessages("user3");
+            foreach (MessageModel msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
+
+                DB.MarkNotRead(msg.Mid);
+            }
+            Console.WriteLine("______________________________________________________________________________________________");
+            x = DB.GetMessages("user3");
+            foreach (MessageModel msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
+
+                DB.Delete(msg.Mid);
+            }
+            Console.WriteLine("______________________________________________________________________________________________");
+            x = DB.GetMessages("user3");
+            foreach (MessageModel msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
+
+                DB.Restore(msg.Mid);
+            }
+            Console.WriteLine("______________________________________________________________________________________________");
+            x = DB.GetMessages("user3");
+            foreach (MessageModel msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
             }
 
             using (ServiceHost host = new ServiceHost(typeof(MessageService.MessageService)))
