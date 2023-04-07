977b26abe6d67921498567a91f8ea16152147474
jamesw1123 <jamesw11233@gmail.com>
2023-04-07 12:29:17 -0500

    Added a main function to start the service, and test comms to the db.

---
+++ Host/HostMain.cs
@@ -0,0 +1,101 @@
+﻿using DataComm;
+using DataComm.DbAccess;
+using System;
+using System.ServiceModel;
+
+namespace Host
+{
+    internal class HostMain
+    {
+        private static void Main(string[] args)
+        {
+            //MessageModel message = new MessageModel();
+            //message.Name = "Anyone Else";
+            //message.Address = "Some St.";
+            //message.Phone = "987-987-9876";
+            //message.Email = "anyone@email.com";
+            //message.Message_Text = "ANY message can go here";
+            //message.Date_Taken = DateTime.Now.ToString();
+            //message.Recipient = "user3";
+            //message.Sent_From = "user1";
+            //DB.Insert(message);
+
+            var x = DB.GetAllMessages("user3");
+            foreach (Message msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Recipient);
+                Console.WriteLine(msg.Name);
+                Console.WriteLine(msg.Address);
+                Console.WriteLine(msg.Phone);
+                Console.WriteLine(msg.Email);
+                Console.WriteLine(msg.Message_Text);
+                Console.WriteLine(msg.Date_Taken);
+                Console.WriteLine(msg.Sent_From);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
+
+                //DB.MarkRead(msg.Mid);
+            }
+            Console.WriteLine("______________________________________________________________________________________________");
+            x = DB.GetNewMessages("user3");
+            foreach (Message msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Name);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
+
+                //DB.MarkNotRead(msg.Mid);
+            }
+            Console.WriteLine("______________________________________________________________________________________________");
+            x = DB.GetDeletedMessages("user3");
+            foreach (Message msg in x)
+            {
+                Console.WriteLine(msg.Mid);
+                Console.WriteLine(msg.Name);
+                Console.WriteLine(msg.Delivered);
+                Console.WriteLine(msg.Read);
+                Console.WriteLine(msg.Deleted);
+                Console.WriteLine();
+
+                //DB.Delete(msg.Mid);
+            }
+            Console.WriteLine("______________________________________________________________________________________________");
+            //x = DB.GetMessages("user3");
+            //foreach (MessageModel msg in x)
+            //{
+            //    Console.WriteLine(msg.Mid);
+            //    Console.WriteLine(msg.Name);
+            //    Console.WriteLine(msg.Delivered);
+            //    Console.WriteLine(msg.Read);
+            //    Console.WriteLine(msg.Deleted);
+            //    Console.WriteLine();
+
+            //    //DB.Restore(msg.Mid);
+            //}
+            //Console.WriteLine("______________________________________________________________________________________________");
+            //x = DB.GetMessages("user3");
+            //foreach (MessageModel msg in x)
+            //{
+            //    Console.WriteLine(msg.Mid);
+            //    Console.WriteLine(msg.Name);
+            //    Console.WriteLine(msg.Delivered);
+            //    Console.WriteLine(msg.Read);
+            //    Console.WriteLine(msg.Deleted);
+            //    Console.WriteLine();
+            //}
+
+            using (ServiceHost host = new ServiceHost(typeof(MessageService.MessageService)))
+            {
+                host.Open();
+                Console.WriteLine("Host started @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
