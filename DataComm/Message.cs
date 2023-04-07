a6e5edd13546e1f4559368dd8a2e78b68eb03b90
jamesw1123 <jamesw11233@gmail.com>
2023-04-07 12:25:46 -0500

    Added a class for a Message object

---
+++ DataComm/Message.cs
@@ -0,0 +1,18 @@
+﻿namespace DataComm
+{
+    public class Message
+    {
+        public string Address { get; set; }
+        public string Date_Taken { get; set; }
+        public int Deleted { get; set; }
+        public int Delivered { get; set; }
+        public string Email { get; set; }
+        public string Message_Text { get; set; }
+        public int Mid { get; private set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public int Read { get; set; }
+        public string Recipient { get; set; }
+        public string Sent_From { get; set; }
+    }
+}
