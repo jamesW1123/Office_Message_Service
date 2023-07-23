b9f6af96fd40160924b20f317b0529c855ec1762
jamesw1123 <jamesw11233@gmail.com>
2023-07-23 11:28:11 -0500

    Added a config class

---
+++ DataComm/Config.cs
@@ -0,0 +1,9 @@
+﻿namespace DataComm
+{
+    public class Config
+    {
+        public string ServerIP { get; set; }
+        public string ServerPort { get; set; }
+        public string UserId { get; set; }
+    }
+}
